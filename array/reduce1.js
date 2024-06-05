73b5f8c08a8a64ce49d7e00aceb0275b82e97dba
Adelson de Souza <desouzaadelson@hotmail.com>
2024-06-05 11:39:26 -0300

    reduce1.js

---
+++ array/reduce1.js
@@ -0,0 +1,18 @@
+const alunos = [
+    {name: 'João', nota: 7.3, bolsista: false},
+    {name: 'Maria', nota: 9.2, bolsista: true},    
+    {name: 'Pedro', nota: 9.8, bolsista: false},
+    {name: 'Ana', nota: 8.7, bolsista: true},    
+]
+console.log(alunos.map(a => a.nota)) // Return [ 7.3, 9.2, 9.8, 8.7 ]
+const resultado = alunos.map(a => a.nota).reduce(function(acumulador, atual){
+    console.log(acumulador, atual)  // return 10 7.3
+    return acumulador + atual
+}, 10)                              // Acumulador
+/**
+ * return 17.3 9.2
+ *        26.5 9.8
+ *        36.3 8.7
+ */
+
+console.log(resultado) // 45
