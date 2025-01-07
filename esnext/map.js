93f19aac831078df2db014192c952955ab3c72a5
Adelson de Souza <desouzaadelson@hotmail.com>
2025-01-07 16:56:47 -0300

    map

---
+++ esnext/map.js
@@ -0,0 +1,22 @@
+// AULA 
+
+/**
+ * TODO: See the links:
+ * 
+ ** https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+ * 
+ */
+
+const tecnologias = new Map()
+tecnologias.set('react', { framework: false })
+tecnologias.set( 'angular', { framework: true } )
+
+console.log(tecnologias.react) // output undefined
+console.log(tecnologias.get('react')) //FORMA CORRETA output { framework: false }
+console.log(tecnologias.get('react').framework) //FORMA CORRETA output false
+
+const chavesVariadas = new Map( [ 
+    [ function () { }, 'Função'],
+    [ { }, 'Objeto' ],
+    [ 123, 'Numero'],
+ ] )
