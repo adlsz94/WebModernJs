9ef21ad58ab7a55a6c69e262c4d748aef6987f41
Adelson de Souza <desouzaadelson@hotmail.com>
2025-01-27 18:02:27 -0300

    usandoCallback, usandoPromises

--- esnext/usandoCallback.js
+++ esnext/usandoCallback.js
@@ -2,6 +2,7 @@
 const http = require('http')
 
 const getTurma = (letra, callback) => {
+    // template string
     const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
     http.get(url, res => {
         let resultado = ''
---
+++ esnext/usandoPromises.js
@@ -0,0 +1,64 @@
+/**
+ *? Promise
+ * 
+ * TODO: See the links below:
+ * 
+ ** https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Promise
+ * 
+ ** https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Using_promises
+ * 
+ */
+
+ // Com promise
+ const { error } = require('console')
+const http = require('http')
+const { reject } = require('lodash')
+const { resolve } = require('path')
+ 
+ const getTurma = (letra) => {
+     // template string
+     const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
+     return new Promise((resolve, reject) => {
+        http.get(url, res => {
+            let resultado = ''
+
+            res.on('data', dados => {
+                resultado += dados
+            })
+            
+            res.on('end', () => {
+                try {
+                    resolve(JSON.parse(resultado))
+                } catch (error) {
+                    reject(error)
+                }
+ 
+            })
+        })
+     })
+ }
+ 
+ /**
+    let nomes = []
+    getTurma('A').then(alunos => {
+        nomes = nomes.concat(alunos.map(a => `A: ${a.nome}`))
+        
+        getTurma('B').then(alunos => {
+            nomes = nomes.concat(alunos.map(a => `B: ${a.nome}`))
+            
+            getTurma('C').then(alunos => {
+                nomes = nomes.concat(alunos.map(a => `C: ${a.nome}`))
+                console.log(nomes)
+            })
+        })
+    })
+ */
+
+ Promise.all([getTurma('A'), getTurma('B'), getTurma('C')])
+    
+    .then(turmas => [].concat(...turmas))
+    .then(alunos => alunos.map(aluno => aluno.nome))
+    .then(nomes => console.log(nomes))
+    .catch(err => console.log(err))
+
+    getTurma('D').catch(error => console.log(error.message))
