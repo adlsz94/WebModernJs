895b8059c3476151ff8c398facb6fef41c60de58
Adelson de Souza <desouzaadelson@hotmail.com>
2025-02-14 12:31:40 -0300

    dados1Txt, desafio_3, 2 arquivos lidos

--- esnext/desafio_3.js
+++ esnext/desafio_3.js
@@ -1,31 +1,27 @@
 // AULA 192 DESAFIO PROMISE / AULA 193 RESPOSTA
 
+const fs = require('fs')
+const path = require('path')
+
+async function lerArquivo() {
+  try {
+    const caminho = path.join(__dirname, 'dados1.txt')// procura o arquivo no diretorio atual.
+    const conteudo = await fs.promises.readFile(caminho, 'utf8')// Lerá o arquivo no diretorio
+    console.log(conteudo)
+  } catch (err) {
+    console.error('Erro ao ler o arquivo:', err.message)
+  }
+}
+
+lerArquivo()
 /**
-    const fs = require('fs')
-    
-    const path = require('path')
-    
-    
-    function lerArquivo(caminho) {
-        return new Promise(resolve => {
-            
-            fs.readFile(caminho, function(_, conteudo) {
-                
-                resolve(conteudo.toString())
-                
-            })
-            console.log('')
-        })
-    }
-    
-    const caminho = path.join(__dirname, 'dados.txt')
-    
-    lerArquivo(caminho)
-        .then(conteudo => conteudo.split('\n'))
-        .then(linhas => linhas.join('.'))
-        .then(conteudo => `O valor final é: ${conteudo}`)
-        .then(console.log)
-*/
+ * Expected output:
+ * Desafio_3
+ * Resolvido?
+ * Sim, após testar várias maneiras.
+ * 
+ */
+
 
 /**
  * TODO: See the links below
@@ -34,9 +30,10 @@
  * 
  */
 
+
 const { readFile } = require('node:fs/promises');
 const { resolve } = require('node:path');
-async function lerArquivo() {
+async function lerArquivo1() {
   try {
     const filePath = resolve('./dados.txt');
     const contents = await readFile(filePath, { encoding: 'utf8' });
@@ -45,4 +42,12 @@ async function lerArquivo() {
     console.error(err.message);
   }
 }
-lerArquivo();
+
+lerArquivo1();
+/**
+ * Expected output:
+ * Linha 1
+ * linha 2
+ * linha 3
+ * 
+ */
